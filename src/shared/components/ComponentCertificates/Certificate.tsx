aa47d9f6da96540ffaa1e58e34d0b599dbef919d
Voleidol <fhdha@yandex.ru>
2023-08-25 14:41:48 +0300

    add slider certificate in progress

--- src/shared/components/ComponentCertificates/Certificate.tsx
+++ src/shared/components/ComponentCertificates/Certificate.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import prevArrow from '../../../icons/leftArrow.svg';
+import nextArrow from '../../../icons/rightArrow.svg';
 import styles from './certificate.css';
 
 export function Certificate() {
   return (
     <div className={styles.certificate}>
-      <h1>СЕРТИФИКАТЫ</h1>
+
+      <div className={styles.certificateTitle}>
+        <h2>СЕРТИФИКАТЫ</h2>
+        <div className={styles.certificateTitleDescription}>
+          Основным направлением компании является <span className={styles.descriptionBlue}>производство и комплектация трубопроводных систем</span> для газовой, нефтяной, атомной и нефтехимической промышленности
+        </div>
+      </div>
+
+      <div className={styles.certificateSlider}>
+          <div className={styles.certificateSliderPrevArrow}>
+            <img src={prevArrow} alt="prev" />
+          </div>
+          {/* сделать сам слайдер  */}
+          <div className={styles.certificateSliderNextArrow}>
+            <img src={nextArrow} alt="next" />
+          </div>
+      </div>
     </div>
   );
 }
