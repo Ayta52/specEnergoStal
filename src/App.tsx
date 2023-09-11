49b60a0cdfd41754da8f1b8946bc4fb8e430b6e8
Roman <fhdha@yandex.ru>
2023-09-12 00:03:28 +0300

    add pochti vse

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { Layout } from "./shared/Layout";
+import Router from "./shared/components/ui/Router";
 import { Footer } from "./shared/components/Footer";
 import { Header } from "./shared/components/Header";
-import Router from "./shared/components/ui/Router";
-import "./main.global.css";
 import { Home } from "./shared/components/Home";
 import { Contact } from "./shared/components/Contact";
 import { PreFooter } from "./shared/components/PreFoot";
 import { CertificateList } from "./shared/components/CertificateList";
 import { NotFound } from "./shared/components/NotFound";
+import { ProductList } from "./shared/components/ProductList";
+import { ProductInfo } from "./shared/components/ProductInfo";
+import "./main.global.css";
 
 function AppComponent() {
   return (
@@ -19,7 +21,9 @@ function AppComponent() {
       {/* <Home /> */}
       {/* <Contact /> */}
       {/* <CertificateList /> */}
-      <NotFound />
+      {/* <NotFound /> */}
+      {/* <ProductList /> */}
+      <ProductInfo />
       <PreFooter />
       <div className="border_bottom"></div>
       <Footer />
---
+++ src/shared/components/ProductList/ProductList.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Otvod from "../../../img/productImg/Otvod.svg";
+import Perehod from "../../../img/productImg/Perehod.svg";
+import Troynik from "../../../img/productImg/Troynik.svg";
+import Flanec from "../../../img/productImg/Flanec.svg";
+import Dno from "../../../img/productImg/Dno.svg";
+import Zaglushka from "../../../img/productImg/Zaglushka.svg";
+import Opora from "../../../img/productImg/Opora.svg";
+import Shtucer from "../../../img/productImg/Shtucer.svg";
+import ZapornayaArmatura from "../../../img/productImg/ZapornayaArmatura.svg";
+import backgroundImg from "../../../img/productImg/BackgroundProductList.svg";
+import styles from "./productList.css";
+
+const productListMass = [
+  {
+  id: 1,
+  name: 'Отводы',
+  image: Otvod},
+  {
+  id: 2,
+  name: 'переходы',
+  image: Perehod},
+  {
+  id: 3,
+  name: 'тройники',
+  image: Troynik},
+  {
+  id: 4,
+  name: 'фланцы',
+  image: Flanec},
+  {
+  id: 5,
+  name: 'днища',
+  image: Dno},
+  {
+  id: 6,
+  name: 'заглушки',
+  image: Zaglushka},
+  {
+  id: 7,
+  name: 'опоры',
+  image: Opora},
+  {
+  id: 8,
+  name: 'штуцеры',
+  image: Shtucer},
+  {
+  id: 9,
+  name: 'запорная арматура',
+  image: ZapornayaArmatura},
+];
+
+export function ProductList() {
+  return (
+    <div className={styles.container}>
+      <h1>Продукция</h1>
+      <div className={styles.productList}>
+        <div className={styles.productItem}>
+          {productListMass.map((item, index) => {
+            return (
+              <div
+                className={styles.productItemFlex}
+                key={index}
+              >
+                <img src={item.image} alt={item.name} />
+                <p>{item.name}</p>
+              </div>
+            );
+          })}
+          <div className={styles.backgroundImg}>
+            <img src={backgroundImg} alt="backgroundImg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
