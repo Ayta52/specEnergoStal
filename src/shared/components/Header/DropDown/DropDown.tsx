02173926b917cb7c310a7bcf7c8fb808a6f17356
Roman <fhdha@yandex.ru>
2023-10-03 23:45:31 +0300

    add dropdown

--- src/shared/components/Header/DropDown/DropDown.tsx
+++ src/shared/components/Header/DropDown/DropDown.tsx
@@ -1,13 +1,43 @@
-// import React from 'react';
 import { Link } from "react-router-dom";
 import styles from "./dropDown.module.css";
 
 export function DropDown() {
   return (
     <>
-      <Link to="/" className={styles.navLink}>
-        Главная
-      </Link>
+      <div className={styles.dropdown}>
+        <Link to="/productlist" className={styles.navLink}>
+          Продукция
+        </Link>
+        <ul className={styles.dropdownList}>
+          <li className={styles.dropdownItem}>
+            <Link to="/otvod">Отводы</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/perehod">Переходы</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/troynik">Тройники</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/flanec">Фланцы</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/zaglushka">Заглушки</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/dno">Днища</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/opora">Опоры</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/shtucer">Штуцеры</Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link to="/zapornayaArmatura">Запорная арматура</Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
