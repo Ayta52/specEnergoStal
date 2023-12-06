b86f04dde6bc25f71129c62e280c82f251e9d965
Roman <fhdha@yandex.ru>
2023-12-06 22:24:31 +0300

    add error iphone

--- src/shared/components/Home/PreHeader/PreHeader.tsx
+++ src/shared/components/Home/PreHeader/PreHeader.tsx
@@ -5,101 +5,19 @@ import styles from "./preheader.module.css";
 export function PreHeader() {
   const { width, isScreenPreheaderBR, isScreenPreheaderBR400 } = useResize();
   return (
-    // <>
-    //   <div className={styles.container}>
-    //     <div className={styles.title}>
-    //       {isScreenPreheaderBR ? (
-    //         <h2 className={styles.title}>
-    //         Производство{isScreenPreheaderBR400 ? '' : <br/>} и комплектация{'\n'}{' '}
-    //         <span className={styles.titleBlue}>трубопроводных систем</span>
-    //         {'\n'} <br/> для промышленности
-    //       </h2>
-    //       ) : (
-    //         <h2 className={styles.title}>
-    //         Производство и комплектация{'\n'}{' '}
-    //         <span className={styles.titleBlue}>трубопроводных систем</span>
-    //         {'\n'} <br/> для промышленности
-    //       </h2>
-    //       )
-    //       }
-    //     </div>
-
-    //     <div className={styles.rightBlok}>
-    //       <button className={styles.downloadBtn}>
-    //         <svg
-    //           className={styles.downloadBtnIcon}
-    //           width="192"
-    //           height="58"
-    //           viewBox="0 0 192 58"
-    //           fill="none"
-    //           xmlns="http://www.w3.org/2000/svg"
-    //         >
-    //           <path
-    //             d="M96 56L4 29L96 2L188 29L96 56Z"
-    //             stroke="#008FFF"
-    //             strokeWidth="2"
-    //             strokeMiterlimit="10"
-    //           />
-    //         </svg>
-    //         <p className={styles.downloadBtnText}>скачать буклет</p>
-    //       </button>
-    //       <div className={styles.rightDescr}>
-    //         Обеспечиваем потребности{'\n'} предприятий и заводов России{'\n'} и
-    //         стран СНГ с 2006 года
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
-
-    // <>
-    //   <div className={styles.container}>
-
-    //     <div className={styles.title}>
-    //       {isScreenPreheaderBR ? (
-    //         <h2 className={styles.title}>
-    //           Производство{isScreenPreheaderBR400 ? "" : <br className={styles.br}/>} и комплектация
-    //           {"\n"}{" "}
-    //           <span className={styles.titleBlue}>трубопроводных систем</span>
-    //           {"\n"} <br className={styles.br}/> для промышленности
-    //         </h2>
-    //       ) : (
-    //         <h2 className={styles.title}>
-    //           Производство и комплектация{"\n"}{" "}
-    //           <span className={styles.titleBlue}>трубопроводных систем</span>
-    //           {"\n"} <br className={styles.br}/> для промышленности
-    //         </h2>
-    //       )}
-    //     </div>
-
-    //     <div className={styles.rightDescr}>
-    //       {width <= 400 ?
-    //         <>Обеспечиваем потребности предприятий и заводов России и стран СНГ с 2006 года</>
-    //         :
-    //         <>
-    //           <p>Обеспечиваем потребности</p>
-    //           <p>предприятий и заводов </p>
-    //           <p>России и стран СНГ с 2006 года</p>
-    //         </>
-    //       }
-    //     </div>
-
-    //   </div>
-    // </>
-
-    //  Последний вариант
     <>
       <div className={styles.container}>
         <div className={styles.titleDiv}>
           <h2 className={styles.title}>
-            Производство и&nbsp;комплектация{"\n"}{" "}
-            <span className={styles.titleBlue}>трубопроводных систем</span>
-            {"\n"} для&nbsp;промышленности
+            Производство и&#160;комплектация{"\n"}{" "}
+            <span className={styles.titleBlue}>трубопроводных&#160;систем</span>
+            {"\n"} для&#160;промышленности
           </h2>
         </div>
 
         <div className={styles.rightDescr}>
           <p className={styles.rightDescrP}>Обеспечиваем потребности {"\n"}
-            предприятий и заводов&nbsp;России {"\n"}
+            предприятий и заводов&#160;России {"\n"}
             и стран СНГ с 2006 года</p>
         </div>
       </div>
